b5ca6b4e886d34bc76145749b8257fec37fdb07c
Daniel Olagunju <todak2000@gmail.com>
2023-02-15 15:41:48 +0100

    remove methods from main.js

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,5 @@ import './index.css'
 export const withdrawWidget = initializeWithdrawalWidget();
 export const depositWidget = initializeDepositWidget();
 console.log("Cowry Protocol Widget script running on your application!")
-withdrawWidget.show();
-depositWidget.show();
+
 
