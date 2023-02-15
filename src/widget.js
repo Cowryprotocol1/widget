1d30b7ab1cfd7dc2d04d26e7856a24c8ddc54993
Daniel Olagunju <todak2000@gmail.com>
2023-02-15 13:46:57 +0100

    refactor

--- src/widget.js
+++
@@ -1,7 +0,0 @@
-
-import { initializeDepositWidget } from './js/Deposit';
-import { initializeWithdrawalWidget } from './js/Withdraw';
-import './index.css'
-
-export const withdrawWidget = initializeWithdrawalWidget();
-export const depositWidget = initializeDepositWidget();
