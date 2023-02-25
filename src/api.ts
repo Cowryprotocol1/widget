c885721266e7cb968cee2928558a9e691f03e503
Afolabi Sunday <sundayafolabi992@gmail.com>
2023-02-25 00:14:13 +0000

    updated api for testing

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
 
 
-const url = `https://cowry-backend.herokuapp.com`
+// const url = `https://cowry-backend.herokuapp.com`
+const url = `http://127.0.0.1:8000`
 
 export const getDepositIntent = async (data: any)=> {
     let rData = {
