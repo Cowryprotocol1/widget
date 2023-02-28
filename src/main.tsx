fc10781f4a6d47b4bc0cfbe48a9b8cac19605ddb
Afolabi Sunday <sundayafolabi992@gmail.com>
2023-02-28 04:22:28 +0000

    updated js file

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import { initializeWithdrawalWidget } from './Withdraw'
 import { initializeDepositWidget } from './Deposit';
 import './index.css'
 
-export const withdrawWidget = initializeWithdrawalWidget();
+// export const withdrawWidget = initializeWithdrawalWidget();
 export const depositWidget = initializeDepositWidget();
 console.log("Cowry Protocol Widget script running on your application! Thanks****")
 
 
-withdrawWidget.show()  // uncomment for withdraw
+// withdrawWidget.show()  // uncomment for withdraw
 depositWidget.show()  // uncoment for depost
